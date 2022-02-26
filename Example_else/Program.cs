14c93637e1e2a3a6eec5cf08cbdad36fc5931742
Darya Ilina <ilinadarya.mow@bk.ru>
2022-02-26 16:22:35 +0300

    Example_else

---
+++ Example_else/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Your name:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "dasha")
+{
+    Console.WriteLine("Cool, that`s Dasha!");
+
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
